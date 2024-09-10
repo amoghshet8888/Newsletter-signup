699a08bc3fa0164a4081710b10ac77cd0f5a7a30
amoghshet8888 <101241995+amoghshet8888@users.noreply.github.com>
2024-09-10 11:33:50 +0530

    Initial commit with secure environment variables

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
 const https = require("https");
+require('dotenv').config(); // Load environment variables
 
 const app = express();
 
@@ -29,12 +29,12 @@ app.post("/", (req, res) => {
 
     const jsonData = JSON.stringify(data);
 
-    const url = "https://us14.api.mailchimp.com/3.0/lists/7fd0338534"
+    const url = `https://us14.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_LIST_ID}`;
 
     const option = {
         method: "POST",
-        auth: "amogh1:46920a73d649f5f519bc8dae19a4c926-us14"
-    }
+        auth: `amogh:${process.env.MAILCHIMP_API_KEY}`
+    };
 
     const request = https.request(url, option, (response) => {
         if(response.statusCode == 200){
@@ -46,27 +46,21 @@ app.post("/", (req, res) => {
 
         response.on("data", (data) => {
             console.log(JSON.parse(data));
-        })
-    })
+        });
+    });
 
     request.write(jsonData);
     request.end();
-})
+});
 
 app.post("/failure", (req, res) => {
     res.redirect("/");
-})
+});
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/signup.html");
-})
+});
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
-})
-
-//api key
-//46920a73d649f5f519bc8dae19a4c926-us14
-
-//List id
-//7fd0338534
+});
